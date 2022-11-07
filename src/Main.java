22ffe747d40a1a8ec501eca6efe53d7e402a538c
Vitor Gallucci <vitorbg04@gmail.com>
2022-11-07 16:37:23 -0300

    Refatorado o main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        sayHello();
+    }
+
+    public static void sayHello () {
         System.out.println("Hello world!!");
     }
+
 }
